5b42996f10e1d8bdb131f8d73b83ae12758e9676
Kanishka Rathnayake <kanishkadilshan861@gmail.com>
2024-09-28 01:27:54 +0530

    saved to my own directory

--- new.py
+++ new.py
@@ -14,22 +14,23 @@ def find_job():
     title_name = input('>')
     print(f"filtering for {title_name.strip()}")
 
-    for job in jobs:
+    for index, job in enumerate(jobs):
 
         title = job.header.h2.a.text
         date = job.find('span', class_='sim-posted').text
         if 'few' in date and title_name in title:
-
-            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-            skill = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skill = job.find('span', class_='srp-skills').text.replace(' ', '')
             link = job.header.h2.a['href']
+            with open(f'Posts/{index}.txt', 'w') as f:
 
-            print(f"title: {title.strip()}")
-            print(f"Date: {date.strip()}")
-            print(f"Company Name: {company_name.strip()}")
-            print(f"Skills required: {skill.strip()}")
-            print(f"Link to post {link}")
-            print(" ")
+                f.write(f"title: {title.strip()} \n")
+                f.write(f"Date: {date.strip()} \n")
+                f.write(f"Company Name: {company_name.strip()} \n")
+                f.write(f"Skills required: {skill.strip()} \n")
+                f.write(f"Link to post {link} \n")
+            print(f'File Saved: {index}')
+
 
 
 if __name__ == '__main__':
